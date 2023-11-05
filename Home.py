b957c9a2c44e88a092f7d9e0c91b2bfecb5166aa
devilkiller-ag <ashmitgupta.official@gmail.com>
2023-11-05 19:18:26 +0530

    changed project name

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Page Config
-st.set_page_config(page_title='Project: Shashi Kumar Sahu', page_icon=":cat:", layout='wide')
+st.set_page_config(page_title='Design Of Piston', page_icon=":cat:", layout='wide')
 
 
-st.title("Project: Shashi Kumar Sahu")
+st.title("Design of Piston")
